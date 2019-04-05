a679d0710891e25cf7f5afa58c3fda0d9541b94e
samcherinet <32940167+samcherinet@users.noreply.github.com>
2019-04-05 14:05:07 -0500

    Update SingleSwitch.py
    dumbell topo implementation

--- SingleSwitch.py
+++ SingleSwitch.py
@@ -6,12 +6,25 @@ from mininet.log import setLogLevel
 class SingleSwitchTopo(Topo):
     "Single switch connected to n hosts."
     def build(self, n=20):
-        switch = self.addSwitch('s1')
-        # Python's range(N) generates 0..N-1
-        for h in range(n):
-            host = self.addHost('h%s' % (h + 1))
-            self.addLink(host, switch)
-
+        switch_sender_access = self.addSwitch('sa1')
+        switch_sender_root = self.addSwitch('sr1')
+        self.addLink(switch_sender_access, switch_sender_root)
+        
+        switch_receiver_access = self.addSwitch('ra1')
+        switch_receiver_root = self.addSwitch('rr1')
+        self.addLink(switch_receiver_access,switch_receiver_root)
+        
+        self.addLink(switch_sender_root,switch_receiver_root)
+        
+        host_sender_1 = self.addHost('hs1')
+        self.addLink(host_sender_1, switch_sender_access)
+        host_sender_2 = self.addHost('hs2')
+        self.addLink(host_sender_2, switch_sender_access)
+        host_receiver_1 = self.addHost('hr1')
+        self.addLink(host_receiver_1, switch_receiver_access)
+        host_receiver_2 = self.addHost('hr2')
+        self.addLink(host_receiver_2, switch_receiver_access)
+        
 def simpleTest():
     "Create and test a simple network"
     topo = SingleSwitchTopo(n=20)
