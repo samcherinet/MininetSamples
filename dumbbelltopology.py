d5507b9a233dc10ac71beaf26ca6e03189233e13
samcherinet <32940167+samcherinet@users.noreply.github.com>
2019-04-06 23:39:48 -0500

    forgot to add cli

--- dumbbelltopology.py
+++ dumbbelltopology.py
@@ -2,6 +2,7 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
+from mininet.cli import CLI
 
 class DumbbellTopology(Topo):
     "4 hosts 4 switches."
