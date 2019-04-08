ff45554fdb9b10106da597ac5121b7039a3f9d54
samcherinet <32940167+samcherinet@users.noreply.github.com>
2019-04-07 21:15:32 -0500

    Update dumbbelltopology.py

--- dumbbelltopology.py
+++ dumbbelltopology.py
@@ -13,6 +13,7 @@ class DumbbellTopology(Topo):
         
     "4 hosts 4 switches."
     def build(self):
+        print _delay
         switch_sender_access = self.addSwitch('sa1'+_delay)
         switch_sender_root = self.addSwitch('sr1'+_delay)
         self.addLink(switch_sender_access, switch_sender_root)
