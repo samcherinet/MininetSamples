80ef82566f035334d0ba54f4d6f1012a7cbf8b22
samcherinet <32940167+samcherinet@users.noreply.github.com>
2019-04-07 21:09:20 -0500

    clubbing three different delays

--- dumbbelltopology.py
+++ dumbbelltopology.py
@@ -5,39 +5,43 @@ from mininet.log import setLogLevel
 from mininet.cli import CLI
 from mininet.link import TCLink
 import time
+from subprocess import Popen, PIPE
 
-class DumbbellTopology(Topo):
+class DumbbellTopology(Topo, _delay):
     "4 hosts 4 switches."
     def build(self):
-        switch_sender_access = self.addSwitch('sa1')
-        switch_sender_root = self.addSwitch('sr1')
+        switch_sender_access = self.addSwitch('sa1'+_delay)
+        switch_sender_root = self.addSwitch('sr1'+_delay)
         self.addLink(switch_sender_access, switch_sender_root)
         
-        switch_receiver_access = self.addSwitch('ra1')
-        switch_receiver_root = self.addSwitch('rr1')
+        switch_receiver_access = self.addSwitch('ra1'+_delay)
+        switch_receiver_root = self.addSwitch('rr1'+_delay)
         self.addLink(switch_receiver_access,switch_receiver_root)
         
-        self.addLink(switch_sender_root,switch_receiver_root,bw=10, delay='15ms')
+        self.addLink(switch_sender_root,switch_receiver_root,bw=10, delay=_delay)
         
-        host_sender_1 = self.addHost('hs1')
+        host_sender_1 = self.addHost('hs1'+_delay)
         self.addLink(host_sender_1, switch_sender_access)
-        host_sender_2 = self.addHost('hs2')
+        host_sender_2 = self.addHost('hs2'+_delay)
         self.addLink(host_sender_2, switch_sender_access)
-        host_receiver_1 = self.addHost('hr1')
+        host_receiver_1 = self.addHost('hr1'+_delay)
         self.addLink(host_receiver_1, switch_receiver_access)
-        host_receiver_2 = self.addHost('hr2')
+        host_receiver_2 = self.addHost('hr2'+_delay)
         self.addLink(host_receiver_2, switch_receiver_access)
         
-def runTest():
+def runTest(_delay='21ms'):
+    print 'running test for a delay of ' + _delay
+    
     "Create and test the network"
-    topo = DumbbellTopology()
+    topo = DumbbellTopology(_delay)
     net = Mininet(topo, link=TCLink)
     net.start()
-    print "Dumping host connections"
-    dumpNodeConnections(net.hosts)
-    print "Testing network connectivity"
     
-    s1, s2, r1, r2 = net.getNodeByName('hs1', 'hs2','hr1', 'hr2')
+    print "Dumping host connections for "+_delay
+    dumpNodeConnections(net.hosts)
+    print "Testing network connectivity for "+_delay
+    
+    s1, s2, r1, r2 = net.getNodeByName('hs1'+_delay, 'hs2'+_delay,'hr1'+_delay, 'hr2'+_delay)
     
     #start server on recievers
     serverArg = 'iperf3 -s -p 5566 -i 1'
@@ -48,7 +52,7 @@ def runTest():
     
     #simulate message from senders 15 apart
     #from s1 to r1'
-    client1Arg = 'iperf3 -c ' + r1.IP() + ' -p 5566 -t 15 -i .1 -J > s1r1.json' 
+    client1Arg = 'iperf3 -c ' + r1.IP() + ' -p 5566 -t 15 -i .1 -J > '+_delay+'_s1r1.json' 
     #add delay
     #config algorithm
     s1.sendCmd(client1Arg)
@@ -57,24 +61,32 @@ def runTest():
     #wait 15 minutes
     #from s2 to r2
     time.sleep(5)
-    client2Arg = 'iperf3 -c ' + r2.IP() + ' -p 5566 -t 15 -i .1 -J > s2r2.json'
+    client2Arg = 'iperf3 -c ' + r2.IP() + ' -p 5566 -t 15 -i .1 -J > '+_delay+'_s2r2.json'
     #add delay
     #config algorithm
     s2.sendCmd(client2Arg)
     print client2Arg
-        
-    #wait for 20 minute to finish testing
-    t = 0
-    while t < 15 :
-        t += 1
-        time.sleep(1)
-    
+
+    return net
     #CLI(net)
-    Popen("killall -9 top bwm-ng", shell=True).wait()
-    net.stop()
 
 if __name__ == '__main__':
     # Tell mininet to print useful information
     setLogLevel('info')
     # _delay='81ms',_delay='162ms'
-    runTest()
+    net1 = runTest('21ms')
+    net2 = runTest('81ms')
+    net3 = runTest('162ms')
+    
+    #wait for 20 minute to finish testing
+    t = 0
+    while t < 30 :
+        t += 1
+        time.sleep(1)
+        print t+' of 30 seconds left'
+    
+    Popen("killall -9 top bwm-ng", shell=True).wait()
+    net1.stop()
+    net2.stop()
+    net3.stop()
+    
